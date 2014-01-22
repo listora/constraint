d947b6fb5f55c66c25faa9001e6f0f7b5e79c11d
James Reeves <jreeves@weavejester.com>
2014-01-22 18:03:10 +0000

    Nest tests for better categorization

--- test/constraint/core_test.clj
+++ test/constraint/core_test.clj
@@ -4,16 +4,17 @@
 
 (deftest test-validate
   (testing "basic types"
-    (is (empty? (validate String "foo"))))
-  (testing "derived types"
-    (is (empty? (validate Number 1)))
-    (is (empty? (validate Number 1.2))))
-  (testing "type error"
-    (is (= (validate Integer 1.2)
-           [{:error :invalid-type
-             :message "data type does not match definition"
-             :expected Integer
-             :found Double}])))
+    (testing "valid"
+      (is (empty? (validate String "foo"))))
+    (testing "derived"
+      (is (empty? (validate Number 1)))
+      (is (empty? (validate Number 1.2))))
+    (testing "error"
+      (is (= (validate Integer 1.2)
+             [{:error :invalid-type
+               :message "data type does not match definition"
+               :expected Integer
+               :found Double}]))))
   (testing "nil"
     (is (empty? (validate nil nil)))
     (is (= (validate nil "foo")
@@ -26,18 +27,19 @@
     (is (empty? (validate (U String nil) nil)))
     (is (not (empty? (validate (U String nil) 10)))))
   (testing "vectors"
-    (is (empty? (validate [String Number] ["foo" 10]))))
-  (testing "collection size"
-    (is (= (validate [String Number] ["foo"])
-           [{:error :invalid-type
-             :message "data type does not match definition"
-             :expected [String Number]
-             :found [String]}])))
-  (testing "collection type"
-    (is (= (validate [String Number] {"foo" 10})
-           [{:error :invalid-type
-             :message "data type does not match definition"
-             :expected clojure.lang.Sequential
-             :found clojure.lang.PersistentArrayMap}])))
-  (testing "item types"
-    (is (not (empty? (validate [String Number] ["foo" "10"]))))))
+    (testing "valid"
+      (is (empty? (validate [String Number] ["foo" 10]))))
+    (testing "size"
+      (is (= (validate [String Number] ["foo"])
+             [{:error :invalid-type
+               :message "data type does not match definition"
+               :expected [String Number]
+               :found [String]}])))
+    (testing "type"
+      (is (= (validate [String Number] {"foo" 10})
+             [{:error :invalid-type
+               :message "data type does not match definition"
+               :expected clojure.lang.Sequential
+               :found clojure.lang.PersistentArrayMap}])))
+    (testing "item types"
+      (is (not (empty? (validate [String Number] ["foo" "10"])))))))
