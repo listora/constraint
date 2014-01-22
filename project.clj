0164bae4e60887b567e1ee244e49340bf4d88692
James Reeves <jreeves@weavejester.com>
2014-01-22 16:51:35 +0000

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject constraint "0.1.0-SNAPSHOT"
+  :description "Data constraint library"
+  :url "https://github.com/listora/constraint"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
