9e7a1ba11d21f72281af416704fd2e2a5b316d48
James Reeves <jreeves@weavejester.com>
2014-02-24 12:58:15 +0000

    Fix coercion tests

--- src/constraint/coercions.clj
+++ src/constraint/coercions.clj
@@ -1,42 +1,28 @@
-(ns constraint.coercions)
+(ns constraint.coercions
+  "Common-place type coercions.")
 
 (defn- failed-coercion [type data]
   {:error    :failed-coercion
    :coercion type
    :found    data})
 
-(defn make-coercion
-  "Construct a coercion that maps data of one type to another type. Takes a
-  :validate predicate to determine if the data can be coerced, and a :coerce
-  function, which returns the coerced value."
-  [[in-type out-type] & {:keys [validate coerce]}]
-  (reify
-    Transform
-    (transform* [_ data]
-      (let [results (transform* in-type data)]
-        (if (seq (:errors results))
-          results
-          (if (validate data)
-            {:value  (coerce data)}
-            {:errors #{(failed-coercion out-type data)}}))))))
-
 (def uuid-pattern
   #"[0-9a-fA-F]{8}(?:-[0-9a-fA-F]{4}){3}-[0-9a-fA-F]{12}")
 
-(def string->uuid
-  (make-coercion [String java.util.UUID]
-    :validate #(re-matches uuid-pattern %)
-    :coerce   #(java.util.UUID/fromString %)))
+(defn string->uuid [s]
+  (if (re-matches uuid-pattern s)
+    {:value (java.util.UUID/fromString s)}
+    {:errors #{(failed-coercion java.util.UUID s)}}))
 
 (def date-time-pattern
   #"^\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z)$")
 
-(def string->date
-  (make-coercion [String java.util.Date]
-    :validate #(re-matches date-time-pattern %)
-    :coerce   #(.getTime (javax.xml.bind.DatatypeConverter/parseDateTime %)))) 
+(defn string->date [s]
+  (if (re-matches date-time-pattern s)
+    {:value (.getTime (javax.xml.bind.DatatypeConverter/parseDateTime s))}
+    {:errors #{(failed-coercion java.util.Date s)}})) 
 
-(def type-coercions
+(def json-coercions
   "A set of standard type coercion rules for data parsed from JSON."
-  {java.util.UUID string->uuid
-   java.util.Date string->date})
+  {[String java.util.UUID] string->uuid
+   [String java.util.Date] string->date})
