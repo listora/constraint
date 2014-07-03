d0d03e324bc4b2206382e8460f75db8b96a3c190
Lauri Pesonen <lauri.pesonen@iki.fi>
2014-07-03 10:45:11 +0100

    Version 0.0.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject listora/constraint "0.0.6"
+(defproject listora/constraint "0.0.7-SNAPSHOT"
   :description "Data constraint library"
   :url "https://github.com/listora/constraint"
   :license {:name "Eclipse Public License"
