ec85a1a2f6d01f5d789db6a702ad1f198fc9026e
Lauri Pesonen <lauri.pesonen@iki.fi>
2014-07-03 10:21:25 +0100

    Bump up the version number and specify deploy-repositories

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject listora/constraint "0.0.5"
+(defproject listora/constraint "0.0.6-SNAPSHOT"
   :description "Data constraint library"
   :url "https://github.com/listora/constraint"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :deploy-repositories [["releases" :clojars]])
